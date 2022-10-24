6c38f6cf8905745c467a3004884a2a344365171f
HackeDalibor <andjelkovicdalibor58@gmail.com>
2022-10-24 09:10:52 +0200

    home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SubjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(SubjectRepository $subjectRepository): Response
     {
         if($this->getUser() === null) {
 
@@ -17,11 +18,11 @@ class HomeController extends AbstractController
 
         } else {
 
-            
-
-            return $this->render('home/index.html.twig', [
-                'controller_name' => 'HomeController',
-            ]);
+            foreach($this->getUser()->getFollowing() as $following) {
+                return $this->render('home/index.html.twig', [
+                    'subjects' => $subjectRepository->findBy(['user' => $following], ['creationDate' => 'DESC'], 15),
+                ]);
+            }
         }
     }
 
